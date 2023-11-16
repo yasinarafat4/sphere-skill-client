451a6f9c4a590786830f1ee797a20b282bbced14
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-11-16 12:46:22 +0600

    Tailwind css and Daisy Ui added done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      Hello World!
-    </div>
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </>
   );
 };
 
